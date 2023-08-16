394a3e48c004b14548884e78af7b1f28bf323bb9
Rifqi Rafif Akbar <rifqi.akbar@xtremax.com>
2023-08-16 16:52:45 +0700

    section 1

--- src/components/component-1/component1.jsx
+++ src/components/component-1/component1.jsx
@@ -3,7 +3,6 @@ export const Component1 = () => {
         <div className="section-1">
 
             <div className="box">
-                <div className="kiri"> april 21st 2021 - 3:18:36</div>
                 <div className="kiri"> SPE / FRONTEND</div>
             </div>
         </div>
