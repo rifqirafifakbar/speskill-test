a48a8398430a0133307fa170e96a66d1d8c75a43
Rifqi Rafif Akbar <rifqi.akbar@xtremax.com>
2023-08-16 17:25:58 +0700

    add time

--- src/components/component-1/component1.jsx
+++ src/components/component-1/component1.jsx
@@ -1,9 +1,14 @@
+import moment from "moment/moment"
+
 export const Component1 = () => {
     return (
         <div className="section-1">
 
             <div className="box">
-                <div className="kiri"> SPE / FRONTEND</div>
+                <div className="wrapper">
+                    <div className="tanggal">{moment().format('MMMM Do, YYYY - HH:mm:ss')}</div>
+                    <div className="kiri"> {`< SPE / FRONTEND >`}</div>
+                </div>
             </div>
         </div>
     )
